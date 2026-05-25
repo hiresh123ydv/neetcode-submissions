75b9d6a6f46cf9c220d2c2ce872483c187eec436
Hiresh Yadav <hireshyadav0056@gmail.com>
2026-05-25 19:05:17 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        # d={}    
+        # for i in range(0,len(s)):
+        #     if s[i] in d:
+        #         d[s[i]]+=1
+        #     else:
+        #         d[s[i]]=1
+        # for j in range(0,len(t)):
+        #     if t[j] in d and d[t[j]]==0:
+        #         return False
+        #     elif t[j] in d:
+        #         d[t[j]]-=1
+        #     else:
+        #         return False
+        # return True     
+        count=[0]*26
+        for i in range(0,len(s)):
+            count[ord(s[i])-ord("a")]+=1
+            count[ord(t[i])-ord("a")]-=1
+        for j in range(0,len(count)):
+            if count[j]!=0:
+                return False
+        return True          
+        
+
+        
